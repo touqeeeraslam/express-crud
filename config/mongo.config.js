2000a5fa2f39113e14dfd7d181ea41c341c06346
Mubashir Maqbool <mubashirmaqbool987@gmail.com>
2022-10-05 12:07:04 +0500

    updated mongo url -

--- config/mongo.config.js
+++ config/mongo.config.js
@@ -1,8 +1,6 @@
 
 
 const mongoose = require('mongoose');
-const dotEnv = require('dotenv');
-dotEnv.config();
-mongoose.connect(process.env.MONGODB_LOCAL).then(() => { console.log('Database connected!') }).catch((err) => { console.log('error in connecting to database',err) });
+mongoose.connect('mongodb+srv://test_user:root@cluster0.5y1hbxf.mongodb.net/test-db?retryWrites=true&w=majority').then(() => { console.log('Database connected!') }).catch((err) => { console.log('error in connecting to database',err) });
 module.exports = { mongoose };
 
